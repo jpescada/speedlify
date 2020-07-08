edd29d7698ed8d413aa1f4dc3e47b8c0212e524f
Joao Pescada <joao@pescada.pt>
2020-07-08 19:41:14 +0100

    Updated sites

--- _data/sites.js
+++ _data/sites.js
@@ -1,7 +1,7 @@
 module.exports = {
-	personal: {
-		name: "personal", // optional, falls back to object key
-		description: "Personal web site",
+	one: {
+		name: "one", // optional, falls back to object key
+		description: "group one websites",
 		options: {
 			frequency: 60 * 24, // 24 hours
 			freshChrome: "site",
@@ -9,12 +9,13 @@ module.exports = {
 		urls: [
 			"https://joaopescada.com/",
 			"https://joaopescada.com/works/",
-			"https://joaopescada.com/articles/"
+			"https://joaopescada.com/articles/",
+			"https://joaopescada.com/about/"
 		]
 	},
-	yummelier: {
-		name: "yummelier", // optional, falls back to object key
-		description: "Yummelier sites",
+	two: {
+		name: "two", // optional, falls back to object key
+		description: "group two websites",
 		options: {
 			frequency: 60 * 24, // 24 hours
 			// if the sites don’t share assets on the same domain, we can reset
@@ -26,9 +27,9 @@ module.exports = {
 			"https://yummelier.com/behind-the-scenes/"
 		]
 	},
-	hosted: {
-		name: "Hosted", // optional, falls back to object key
-		description: "Hosted sites",
+	three: {
+		name: "three", // optional, falls back to object key
+		description: "group three websites",
 		options: {
 			frequency: 60 * 24, // 24 hours
 			// if the sites don’t share assets on the same domain, we can reset
@@ -37,11 +38,35 @@ module.exports = {
 		},
 		urls: [
 			"https://suprasumo.co/",
-			"https://temppo.com/",
+			"https://temppo.com/"
+		]
+	},
+	four: {
+		name: "four", // optional, falls back to object key
+		description: "group four websites",
+		options: {
+			frequency: 60 * 24, // 24 hours
+			// if the sites don’t share assets on the same domain, we can reset
+			// chrome with each run instead of each site in every run (it’s faster)
+			freshChrome: "run"
+		},
+		urls: [
 			"https://anaemiliasilva.com/",
 			"https://filhosdelondres.com/",
 			"https://tidykingdom.com/",
-			"http://milliaoz.com/",
+			"http://milliaoz.com/"
+		]
+	},
+	five: {
+		name: "five", // optional, falls back to object key
+		description: "group five websites",
+		options: {
+			frequency: 60 * 24, // 24 hours
+			// if the sites don’t share assets on the same domain, we can reset
+			// chrome with each run instead of each site in every run (it’s faster)
+			freshChrome: "run"
+		},
+		urls: [
 			"https://ilustrique.com/",
 			"https://mariabaixinha.com/",
 			"https://catarinacoelho.pt/"
